dbe3cc595803f3350b3455cce2f951dfcc01907b
ggomez0 <gaspargomez2000@outlook.com>
2024-01-21 19:41:59 -0300

    search debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,8 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -9,14 +11,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+    // const handleSearch = useDebouncedCallback((term) => {
+     
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    // }
+    // , 300);
   }
+  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -27,10 +35,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         placeholder={placeholder} onChange={(e) => {
           handleSearch(e.target.value);
         }}   
-        defaultValue={searchParams.get('query')?.toString()}
-
-      />
+        defaultValue={searchParams.get('query')?.toString()}/>
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 }
+
